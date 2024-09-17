862933c57129d8ad0462b23efdf21d8ae015175f
Your Name <your_email@example.com>
2024-09-18 01:49:53 +0300

    "Updated App.css to import Roboto font, modified V16Engine component, removed V16Engine.css, updated Button component and styles, and deleted several image files."

--- src/Pages/HomePage/components/V16Engine/V16Engine.jsx
+++ src/Pages/HomePage/components/V16Engine/V16Engine.jsx
@@ -1,8 +1,10 @@
 import React, { useRef, useEffect } from "react";
-import './V16Engine.css';
+import classes from './V16Engine.module.css';
+import Button from '../../../../components/Button';
+import { Link } from "react-router-dom";
 
 export default function V16Engine() {
-	const pairRef = useRef([]);
+  const pairRef = useRef([]);
 
   useEffect(() => {
     const observerOptions = {
@@ -12,8 +14,8 @@ export default function V16Engine() {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry, index) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('visible'); // Добавляем класс при появлении на экране
-          entry.target.style.transitionDelay = `${index * 0.3}s`; // Добавляем задержку для плавности
+          entry.target.classList.add(classes.visible);
+          entry.target.style.transitionDelay = `${index * 0.3}s`;
         }
       });
     }, observerOptions);
@@ -30,43 +32,67 @@ export default function V16Engine() {
       });
     };
   }, []);
+
   return (
-    <div className="grid-container">
-      <div className="pair-container left-align" ref={(el) => (pairRef.current[0] = el)}>
-        <div className="video-item">
+    <div className={classes.gridContainer}>
+      <div className={classes.productsTitle}>
+        <h2>
+          <span>4MOTION</span>
+          <b>Предлагает</b>
+        </h2>
+      </div>
+
+      <div className={`${classes.pairContainer} ${classes.leftAlign}`} ref={(el) => (pairRef.current[0] = el)}>
+        <div className={`${classes.videoItem} ${classes.videoItem1}`}>
           <video autoPlay muted loop>
             <source src={"https://d2ox13tjqpxop5.cloudfront.net/BUGATTI-2024/Models/Tourbillon/03_Technique/comp/Bugatti_Tourbillon_03c_engine_d.mp4"} type="video/mp4" />
           </video>
         </div>
-        <div className="text-item">Новейшие технические решения, качественная диагностика и опытная команда — ваш ключ к непревзойденной динамике и скорости. Превратите свой двигатель в настоящее произведение инженерного искусства.</div>
+        <div className={classes.textItem}>
+          <h1>Увеличение мощности</h1>
+          Новейшие технические решения, качественная диагностика и опытная команда — ваш ключ к непревзойденной динамике и скорости. Превратите свой двигатель в настоящее произведение инженерного искусства.
+        </div>
       </div>
 
-      <div className="pair-container right-align" ref={(el) => (pairRef.current[1] = el)}>
-        <div className="text-item">Подарите своему автомобилю комфорт и чистоту звука с нашей профессиональной шумоизоляцией. Наслаждайтесь кристально чистым звуком любимой музыки и комфортной тишиной на дороге.</div>
-        <div className="video-item">
+      <div className={`${classes.pairContainer} ${classes.rightAlign}`} ref={(el) => (pairRef.current[1] = el)}>
+        <div className={classes.textItem}>
+          <h1>Шумоизоляцию</h1>
+          Подарите своему автомобилю комфорт и чистоту звука с нашей профессиональной шумоизоляцией. Наслаждайтесь кристально чистым звуком любимой музыки и комфортной тишиной на дороге.
+        </div>
+        <div className={`${classes.videoItem} ${classes.videoItem2}`}>
           <video autoPlay muted loop>
             <source src={"https://d2ox13tjqpxop5.cloudfront.net/BUGATTI-2024/Models/Tourbillon/03_Technique/comp/Bugatti_Tourbillon_03a_beauty_underneath_d0.mp4"} type="video/mp4" />
           </video>
         </div>
       </div>
 
-      <div className="pair-container left-align" ref={(el) => (pairRef.current[2] = el)}>
-        <div className="video-item">
+      <div className={`${classes.pairContainer} ${classes.leftAlign}`} ref={(el) => (pairRef.current[2] = el)}>
+        <div className={`${classes.videoItem} ${classes.videoItem3}`}>
           <video autoPlay muted loop>
             <source src={"https://d2ox13tjqpxop5.cloudfront.net/BUGATTI-2024/Models/Tourbillon/04_Form_follows_performance/comp/Bugatti_Tourbillon_04b_aerodynamics_d.mp4"} type="video/mp4" />
           </video>
         </div>
-        <div className="text-item">Улучшите аэродинамику вашего автомобиля для максимальной скорости и экономии топлива. Наши решения уменьшают сопротивление и улучшают управляемость, делая каждую поездку более плавной и эффективной.</div>
+        <div className={classes.textItem}>
+          <h1>Улучшение аэродинамики</h1>
+          Улучшите аэродинамику вашего автомобиля для максимальной скорости и экономии топлива. Наши решения уменьшают сопротивление и улучшают управляемость, делая каждую поездку более плавной и эффективной.
+        </div>
       </div>
 
-			<div className="pair-container right-align" ref={(el) => (pairRef.current[3] = el)}>
-			<div className="text-item">Преобразите интерьер вашего автомобиля с помощью премиум обшивки и отделки. Элегантные материалы и умелые руки создадут уют и стиль, подчеркивая индивидуальность и комфорт в каждой поездке.</div>
-        <div className="video-item">
+      <div className={`${classes.pairContainer} ${classes.rightAlign}`} ref={(el) => (pairRef.current[3] = el)}>
+        <div className={classes.textItem}>
+          <h1>Отделку салона</h1>
+          Преобразите интерьер вашего автомобиля с помощью премиум обшивки и отделки. Элегантные материалы и умелые руки создадут уют и стиль, подчеркивая индивидуальность и комфорт в каждой поездке.
+        </div>
+        <div className={`${classes.videoItem} ${classes.videoItem4}`}>
           <video autoPlay muted loop>
             <source src={"https://d2ox13tjqpxop5.cloudfront.net/BUGATTI-2024/Models/Tourbillon/05_Interior/comp/Bugatti_Tourbillon_05ab_entry_material_d.mp4"} type="video/mp4" />
           </video>
         </div>
       </div>
+
+      <Link className={classes.link} to='/services' ref={(el) => (pairRef.current[4] = el)}>
+        <Button className={classes.V16Button} title="ПОЛНЫЙ КАТАЛОГ" />
+      </Link>
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from '../styles/Button.module.css'
 
-export default function Button() {
+export default function Button({title, className}) {
 	return (
-		<div className={classes.button}>
+		<div className={`${classes.button} ${className}`}>
 		<a className={classes.btn} href="javascript:;" data-fancybox="" data-src="#contacts-modal">
-					<p>Сделай этот шаг</p>
+					<p>{title}</p>
 					<span className={classes.item1}></span>
 					<span className={classes.item2}></span>
 					<span className={classes.item3}></span>      

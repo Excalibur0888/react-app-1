5edcd97a354ce8f9beababf85fb08f0def9fdb8e
Your Name <your_email@example.com>
2024-10-12 23:50:26 +0300

    "Updated Reviews and Contacts components with new CSS styles, refactored Swiper navigation, and removed unnecessary code."

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import './Contacts.css';
+import React, { useRef } from "react";
+import classes from './Contacts.module.css';
+import styles from '../../styles/NavArrows.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import contacts from './contactsData';
@@ -9,36 +10,61 @@ import 'swiper/css/navigation';
 export default function Contacts() {
   const images = Array.from({ length: 8 }, (_, i) => require(`./img/person${i + 1}.webp`));
 
+  // Рефы для кнопок
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
   return (
-    <div className="contacts__container">
+    <div className={classes.contacts__container}>
       <Swiper
         slidesPerView={3}
-        navigation
         loop={true}
         speed={400}
         modules={[Navigation]}
-        className="contacts-swiper"
+        className={classes.contacts_swiper}
+        onInit={(swiper) => {
+          // Инициализация навигации после того, как Swiper будет готов
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
       >
+        <div ref={prevRef} className={styles.custom_swiper_button_prev} style={{ left: '0', top: '40%' }}>
+          <span></span>
+        </div>
+
         {contacts.map((contact, index) => (
-          <SwiperSlide key={index} className="contacts-slide">
-            <div className="contacts__item">
-              <div className="contacts__image" style={{ backgroundImage: `url(${images[index]})` }}></div>
-              <div className="contacts__name">{contact.name}</div>
-              <p className="contacts__description">
-								{contact.description.split('/').map((desc, index) => (
-									<span key={index}>{desc}{index < contact.description.split('/').length - 1 ? <br /> : ''}</span>
-								))}
-							</p>
-              <p className="contacts__hours">
-								Пн-Пт: <span className="hours-highlight">{contact.hours}</span>
-							</p>
-              <div className="phone__container">
-								<i className="whatsapp__icon"></i><p className="contacts__phone">{contact.phone}</p>
-								</div>
+          <SwiperSlide key={index} className={classes.contacts_slide}>
+            <div className={classes.contacts__item}>
+              <div
+                className={classes.contacts__image}
+                style={{ backgroundImage: `url(${images[index]})` }}
+              ></div>
+              <div className={classes.contacts__name}>{contact.name}</div>
+              <p className={classes.contacts__description}>
+                {contact.description.split('/').map((desc, index) => (
+                  <span key={index}>
+                    {desc}
+                    {index < contact.description.split('/').length - 1 ? <br /> : ''}
+                  </span>
+                ))}
+              </p>
+              <p className={classes.contacts__hours}>
+                Пн-Пт: <span className={classes.hours_highlight}>{contact.hours}</span>
+              </p>
+              <div className={classes.phone__container}>
+                <i className={classes.whatsapp__icon}></i>
+                <p className={classes.contacts__phone}>{contact.phone}</p>
+              </div>
             </div>
           </SwiperSlide>
         ))}
+
+        <div ref={nextRef} className={styles.custom_swiper_button_next} style={{ right: '0', top: '40%' }}>
+          <span></span>
+        </div>
       </Swiper>
     </div>
-  )
+  );
 }
